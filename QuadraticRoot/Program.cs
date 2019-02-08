f8b92f525c11144595731136556353385c59611b
Andris Jeršovs 🔰 <andris.jersovs@visma.com>
2019-02-08 10:16:26 +0200

    Add project files.

---
+++ QuadraticRoot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuadraticRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
